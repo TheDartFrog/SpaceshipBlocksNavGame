dad335551408a61a46706c780ae4f76a9cfbcb1c
Dart Frog <30552512+TheDartFrog@users.noreply.github.com>
2026-04-23 22:52:05 +0200

    stuff

--- scripts/global.gd
+++ scripts/global.gd
@@ -3,6 +3,81 @@ extends Node
 func _ready():
 	pass
 
+## !!!!!!!!!!!!!!!!!!
+## When adding new block shapes - add #1 here, #2 in blockTypeToString, #3 in blockDictionary
+## !!!!!!!!!!!!!!!!!!
+enum blockTypesEnums {NONE, I, O, T, J, L, S, Z}
 
 
-enum blockTypes {NONE, I, O, T, J, L, S, Z}
+## 0 = empty, 1 = pass, 2 = wall
+var blockDictionary: Dictionary = {
+	# EMPTY BLOCK
+	blockTypesEnums.NONE: [
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# I BLOCK
+	blockTypesEnums.I: [
+	[1, 0, 0, 0], 
+	[1, 0, 0, 0], 
+	[1, 0, 0, 0], 
+	[1, 0, 0, 0]],
+	
+	# O BLOCK
+	blockTypesEnums.O: [
+	[1, 1, 0, 0], 
+	[1, 1, 0, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# T BLOCK
+	blockTypesEnums.T: [
+	[1, 1, 1, 0], 
+	[0, 1, 0, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# J BLOCK
+	blockTypesEnums.J: [
+	[0, 1, 0, 0], 
+	[0, 1, 0, 0], 
+	[1, 1, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# L BLOCK
+	blockTypesEnums.L: [
+	[1, 0, 0, 0], 
+	[1, 0, 0, 0], 
+	[1, 1, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# S BLOCK
+	blockTypesEnums.S: [
+	[0, 1, 1, 0], 
+	[1, 1, 0, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0]],
+	
+	# Z BLOCK
+	blockTypesEnums.Z: [
+	[1, 1, 0, 0], 
+	[0, 1, 1, 0], 
+	[0, 0, 0, 0], 
+	[0, 0, 0, 0]]}
+
+
+func blockTypeToString(blockTypesToString) -> String:
+	var blockTypeString = "NONE"
+	
+	match(blockTypesToString): # if we need to display name
+		0: blockTypeString = "NONE"
+		1: blockTypeString = "I"
+		2: blockTypeString = "O"
+		3: blockTypeString = "T"
+		4: blockTypeString = "J"
+		5: blockTypeString = "L"
+		6: blockTypeString = "S"
+		7: blockTypeString = "Z"
+	return blockTypeString
--- scripts/upcoming_block_manager_script.gd
+++ scripts/upcoming_block_manager_script.gd
@@ -1,18 +1,54 @@
 extends Node
 
+var rng = RandomNumberGenerator.new()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@onready var blockTemplateScene = preload("uid://btluo5aguj20c")
+
+var nextThreeBlocks = [0, 0, 0]
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _ready() -> void:
+	
+	nextThreeBlocks = [selectOneBlock(), selectOneBlock(), selectOneBlock()]
+	
+	spawnNextBlock()
+	
 
 
 
 
 func selectOneBlock() -> int:
-	var selectedBlock = blockTypes.NONE
+	var selectedBlock = Global.blockTypesEnums.NONE
+	
+	
+	selectedBlock = rng.randi_range(1, Global.blockTypesEnums.values().size())
+	
+	print("Selected block: " + Global.blockTypeToString(selectedBlock))
+	
 	return selectedBlock
+
+## pass a 4x4 TileMapLayer to this function
+func blockPatternInterpreter(blockPatternToInterpret, objectToUpdate):
+	print("Interpretting block: " + Global.blockTypeToString(blockPatternToInterpret))
+	print(Global.blockDictionary[blockPatternToInterpret])
+	
+	
+	
+
+
+func spawnNextBlock():
+	print("Spawning next block...")
+	
+	## TODO: czy tak to spawnować żeby działało na gridzie? xdd
+	var blockToAddInstance = blockTemplateScene.instantiate()
+	add_child(blockToAddInstance)
+	# loop thru the 4x4 grid, setting the tiles to 0, 1 or 2
+	blockPatternInterpreter(nextThreeBlocks[0], blockToAddInstance)
+	
+	
+	
+	
+	
+	
+	
