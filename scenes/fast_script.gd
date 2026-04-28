a3a9d045dfc3e29b986934f06e7976c398c71bba
hjrkami <mc.super999@gmail.com>
2026-04-28 14:59:16 +0200

    White noise shader effect

--- scenes/fast_script.gd
+++ scenes/fast_script.gd
@@ -1,15 +1,23 @@
 extends SubViewportContainer
 
 var dead: bool = false
-
+@onready var white_noise = $SubViewport/WhiteNoiseTest
+@onready var u_dead = $SubViewport/YOUAREDEAD
 
 func _ready() -> void:
 	material.set_shader_parameter("ca_strength", 2.5)
 	material.set_shader_parameter("blur_strength", 0.5)
+	white_noise.material.set_shader_parameter("transparency", 1.0)
+	
 	
 	var turn_on_tween = create_tween()
 	turn_on_tween.tween_property(self, "material:shader_parameter/curvature", 6.0, .75)\
 	.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_OUT)
+	
+	await turn_on_tween.finished
+	
+	var noise_tween = create_tween()
+	noise_tween.tween_property(white_noise, "material:shader_parameter/transparency", 0.0, .5)
 
 
 func _process(delta: float) -> void:
@@ -28,10 +36,15 @@ func reload_game():
 	if dead:
 		return
 	dead = true
+	#u_dead.visible = true
+	
+	var noise_tween = create_tween()
+	noise_tween.tween_property(white_noise, "material:shader_parameter/transparency", 1.0, .1)
+	
+	await get_tree().create_timer(2.0).timeout
 	
 	var turn_off_tween = create_tween()
-	turn_off_tween.tween_property(self, "material:shader_parameter/curvature", 0.0, .75)\
-	.set_trans(Tween.TRANS_CIRC).set_ease(Tween.EASE_IN)
+	turn_off_tween.tween_property(self, "material:shader_parameter/curvature", 0.0, .75)
 	
 	var ca_tween = create_tween()
 	ca_tween.tween_property(self, "material:shader_parameter/ca_strength", 25.0, .25)
